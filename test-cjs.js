678c349bb2f80431206cb40eead01a728d7848ae
sunanmau5 <sunan.regi@gmail.com>
2024-11-16 20:24:23 +0100

    chore: test files from git

--- test-cjs.js
+++
@@ -1,5 +0,0 @@
-const Possessive = require("./dist/index.js");
-
-const possessive = new Possessive();
-console.log(possessive.makePossessive("John")); // Should output: John's
-console.log(possessive.makePossessive("Chris")); // Should output: Chris'
--- test-esm.mjs
+++
@@ -1,5 +0,0 @@
-import Possessive from "./dist/index.mjs";
-
-const possessive = new Possessive();
-console.log(possessive.makePossessive("John")); // Should output: John's
-console.log(possessive.makePossessive("Chris")); // Should output: Chris'
